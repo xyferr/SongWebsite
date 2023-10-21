53972a0f6bd248a5926425985675e2ec693d3a75
Harsh Yadav <hy945196@gmail.com>
2023-10-21 23:10:23 +0530

    Structured File & Added React Router

---
+++ Musica/client/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { Outlet } from 'react-router-dom'
+
+
+const Layout = () => {
+  return (
+    <>
+       <Header />
+       <Outlet/>
+       <Footer />
+    </>  
+  )
+}
+
+export default Layout
